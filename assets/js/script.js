882d704debde260dfcf1cc019c0d11b94310b8cb
xoxkoo <anton.durcak22@gmail.com>
2022-10-11 16:46:47 +0200

    hero section gradient

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,18 +44,32 @@
 //         html.innerHTML = data
 //     }, 500)
 // }
+const overlay = document.getElementById('overlay')
+const icon = document.getElementById('nav-icon')
+
+function hideMenu() {
+	icon.classList.remove('open')
+	document.querySelector('.navigation').classList.remove('open')
+	overlay.classList.remove('display')
+}
+
+function openMenu() {
+	icon.classList.add('open')
+	document.querySelector('.navigation').classList.add('open')
+	overlay.classList.add('display')
+}
+
 document.getElementById('nav-icon').addEventListener('click', function () {
     if(this.classList.contains('open')) {
-        this.classList.remove('open')
-        document.querySelector('.navigation').classList.remove('open')
-      //   if(document.querySelector('header').classList.contains('fixed'))
-      //       document.querySelector('header').style.height = '5rem'
-      //   else
-      //       document.querySelector('header').style.height = '6rem'
-    }
-    else {
-        this.classList.add('open')
-        document.querySelector('.navigation').classList.add('open')
+		hideMenu()
+
+		  //   if(document.querySelector('header').classList.contains('fixed'))
+		  //       document.querySelector('header').style.height = '5rem'
+		  //   else
+		  //       document.querySelector('header').style.height = '6rem'
+		}
+		else {
+			openMenu()
       //   if(document.querySelector('header').classList.contains('fixed'))
       //       document.querySelector('header').style.height = '12rem'
       //   else
@@ -65,7 +79,15 @@ document.getElementById('nav-icon').addEventListener('click', function () {
 
 })
 
-const elements = document.querySelectorAll('.animate')
+document.querySelectorAll('.link').forEach(link => {
+	link.addEventListener('click', () => {
+		if (overlay.classList.contains('display')) {
+			hideMenu()
+		}
+	})
+})
+
+let elements = document.querySelectorAll('.animate')
 let scrolling = false
 
 // on scroll animations
@@ -80,11 +102,13 @@ setInterval(() => {
          //  add header background when scroll down
 			// document.querySelector('header').classList.toggle('fixed', window.scrollY > 0)
 
+			let i = 0
 			elements.forEach(element => {
 				if(window.scrollY + window.innerHeight > element.offsetTop + (window.innerHeight * 0.2)) {
 						element.classList.add('animate-element')
 				}
+				i++
 			})
     }
 
-}, 150);
+}, 250);
